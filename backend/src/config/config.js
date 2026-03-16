9a8e5bfbcd97875dedbedd5abe18508ec8408ccd
Sanvi <sanvikumari19@gmail.com>
2026-03-17 01:17:45 +0530

    fix: set sameSite to none in production for cross-domain cookies

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,7 +1,7 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge:5 * 60 * 60 * 1000, // 5 hours
     path: '/'
 }
